d1be99cb14460a972de4ac73b3115c8913287018
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-25 02:28:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,21 +6,18 @@
 
 def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
-   puts happy_birthday(birthday_kids) =
-   ["Happy Birthday Timmy! You are now 9 years old!","Happy Birthday Sarah! You are now 6 years old!","Happy Birthday Amanda! You are now 27 years old!"]
-  end
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ end
 end
-
- birthday_kids = {
-  "Timmy" => 9, 
-  "Sarah" => 6, 
-  "Amanda" => 27
-  }
   
 def age_appropriate_birthday
+  (birthday_kids)
   birthday_kids.each do |kids_name, age|
-  puts ["Happy Birthday Timmy! You are now 9 years old!","Happy Birthday Sarah! You are now 6 years old!", "Happy Birthday Amanda! You are now 27 years old!"]
+  if age < 12 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else 
+    puts "You are too old for kids"
   end
-  exit 
+end
 end
 
