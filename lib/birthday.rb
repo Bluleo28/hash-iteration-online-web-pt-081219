cd33901441464cc7181021a00fa9b11038b64497
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-25 02:24:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,26 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+  }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+    birthday_kids.each do |kids_name, age|
+   puts happy_birthday(birthday_kids) =
+   ["Happy Birthday Timmy! You are now 9 years old!","Happy Birthday Sarah! You are now 6 years old!","Happy Birthday Amanda! You are now 27 years old!"]
+  end
 end
 
+ birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+  }
+  
+def age_appropriate_birthday
+  birthday_kids.each do |kids_name, age|
+  puts ["Happy Birthday Timmy! You are now 9 years old!","Happy Birthday Sarah! You are now 6 years old!", "Happy Birthday Amanda! You are now 27 years old!"]
+  end
+  exit 
+end
 
